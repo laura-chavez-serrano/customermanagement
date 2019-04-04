32e1364ae1af107a5ddca25835b01ed239e32a12
Laura <laura.s.chavez@outlook.com>
2019-04-03 21:13:12 -0400

    addresslog model to track history of address of customer

---
+++ customerapp/app/models/addresslog.rb
@@ -0,0 +1,2 @@
+class Addresslog < ApplicationRecord
+end
---
+++ customerapp/test/models/addresslog_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AddresslogTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
