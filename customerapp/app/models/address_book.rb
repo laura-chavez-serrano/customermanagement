006b6097ea07baa93fc183b2edb09a1091b3350b
Laura <laura.s.chavez@outlook.com>
2019-04-14 08:11:20 -0400

    table with bootstramp

--- customerapp/app/models/address_book.rb
+++ customerapp/app/models/address_book.rb
@@ -3,7 +3,7 @@ class AddressBook < ApplicationRecord
 has_many :networks, dependent: :destroy
 has_many :apromotions, dependent: :destroy
 has_many :addresslogs, dependent: :destroy
-paginates_per 15
+paginates_per 10
 include PgSearch
 pg_search_scope :search_by_fullname_address_city, :against => [:fullname, :address, :city]
 #  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
