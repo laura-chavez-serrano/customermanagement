fa8fe1ca492f4d1a511956a09b1989748041ee59
Laura <laura.s.chavez@outlook.com>
2019-11-26 11:04:04 -0500

    seed for addressbook

--- customerapp/db/seeds.rb
+++ customerapp/db/seeds.rb
@@ -6,25 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
-AddressBook.delete_all
-CSV.foreach('/Users/laura.s.chavez/Documents/projects/customermanagement/address_book.csv', headers: true, skip_blanks: true) do |row|
-    
 
-    address=AddressBook.new
-    
-         
-        address.first_name = row.to_h["first_name"] 
-        address.last_name = row.to_h["last_name"] 
-        address.address = row.to_h["address"] 
-        address.city = row.to_h["city"] 
-        address.zip = row.to_h["zip"] 
-        address.email = row.to_h["email"]
-        address.status = row.to_h["status"]
-        address.category = row.to_h["category"]
-        address.anniversary = row.to_h["anniversary"]
-        address.comments = row.to_h["comments"]
-        address.customer_type = "Buyer"
-        
-        address.save
-
-end
+CSV.foreach('db/address_books.csv', headers: true) do |row|
+    AddressBook.create!(row.to_h)
+  end
+  
+  ActiveRecord::Base.connection.tables.each do |t|
+    ActiveRecord::Base.connection.reset_pk_sequence!(t)
+  end
